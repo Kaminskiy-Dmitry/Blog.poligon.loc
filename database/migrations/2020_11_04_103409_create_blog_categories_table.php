e5ed37051522178d9b97e905fdeccfe07f32b720
Dmitry-tuktuk <content.manager@gmail.com>
2020-11-04 16:55:15 +0200

    Added tables and connection in database

--- database/migrations/2020_11_04_103409_create_blog_categories_table.php
+++ database/migrations/2020_11_04_103409_create_blog_categories_table.php
@@ -15,7 +15,14 @@ class CreateBlogCategoriesTable extends Migration
     {
         Schema::create('blog_categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('parent_id')->unsigned()->default(0);
+
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('description')->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
