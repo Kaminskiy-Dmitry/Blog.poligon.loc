4ebae1e6de0d56ed9f203e00e1703c5e850e1f44
Dmitry-tuktuk <content.manager@gmail.com>
2020-11-05 13:19:29 +0200

    Added new fake inf in DB(BlogCategory, BlogPost, Users)

---
+++ database/seeds/BlogCaregoriesTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BlogCaregoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $categories = [];
+
+        $cName = 'Без категории';
+        $categories[] = [
+            'title' => $cName,
+            'slug' => str_slug($cName), //The  method generates a URL friendly "slug"
+            'parent_id' => 0,
+        ];
+
+        for($i = 1; $i <= 10; $i++){
+            $cName = 'Категория #'.$i;
+            $parentId = ($i > 4) ? rand(1,4) : 1; //
+
+            $categories[] = [
+                'title' => $cName,
+                'slug' => str_slug($cName),
+                'parent_id' => $parentId,
+            ];
+
+        }
+        DB::table('blog_categories')->insert($categories);
+
+    }
+}
