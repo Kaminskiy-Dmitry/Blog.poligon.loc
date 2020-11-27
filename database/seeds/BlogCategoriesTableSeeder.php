0ebb70db62b35ecf834abf5cc19c040f065923c9
Dmitry-tuktuk <content.manager@gmail.com>
2020-11-27 12:43:50 +0200

    rename seeder and update db

--- database/seeds/BlogCategoriesTableSeeder.php
+++ database/seeds/BlogCategoriesTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class BlogCaregoriesTableSeeder extends Seeder
+class BlogCategoriesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(BlogCaregoriesTableSeeder::class);
+        $this->call(BlogCategoriesTableSeeder::class);
         factory(\App\Models\BlogPost::class, 100)->create();
     }
 }
